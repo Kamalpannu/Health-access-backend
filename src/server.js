78a412b4afd490d4dc84b258c93fa88c8c00a522
preetkamal <preetkamalsingh986@gmail.com>
2025-10-03 05:42:40 -0700

    Logout Config

--- src/server.js
+++ src/server.js
@@ -57,13 +57,16 @@ app.get('/auth/google/callback',
   }
 );
 
-// Logout
 app.get('/logout', (req, res, next) => {
   req.logout(err => {
     if (err) return next(err);
     req.session.destroy(() => {
-      res.clearCookie('sid');
-      res.redirect(FRONTEND_URL);
+      res.clearCookie('sid', { 
+        httpOnly: true, 
+        sameSite: "none", 
+        secure: true 
+      });
+      res.status(200).json({ message: 'Logged out successfully' });
     });
   });
 });
